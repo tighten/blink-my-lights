045297710a79d87694a6b502c21710cda0208252
Matt Stauffer <matt@tighten.co>
2018-02-07 11:35:02 -0500

    Move name into env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Blink Matt's Light</title>
+        <title>Blink {{ config('app.name') }} Lights</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Blink Matt's Light
+                    Blink {{ config('app.name') }} Light
                 </div>
 
                 @if (session()->has('message'))
